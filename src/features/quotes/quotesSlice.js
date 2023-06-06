2b545a4eb3521c2f87f94d490ebdb9dd3d91de22
Marie <108511871+Mary-Wanyama@users.noreply.github.com>
2023-06-06 15:32:30 +0300

    Update quotesSlice.js

--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -5,5 +5,24 @@
 const initialState = [];
 
 export default function quotesReducer(state = initialState, action) {
+  switch action.type{
+    case "add/quote":
+      return{
+        ...state
+        quotes: [...state.quotes, action.payload
+      }
+    case "remove/quote":
+      const newList = state.quotes.filter(quotes=>quotes.id !== action.payload)
+      return{
+        ...state
+        quotes: newList
+      }
+      case "add/votes"
+      count = state.quotes.find(quote=> quote.id === quote.id)
+      return{
+  
+       count++
+  }
+    default:
   return state;
 }
